30e61aaea35a38e33591870acc62ba2216670beb
alex <skyycr@gmail.com>
2023-08-18 11:41:18 +0200

    perfection overload

--- fishing.py
+++ fishing.py
@@ -13,31 +13,40 @@ REGION=(841, 66,67, 34)
 print(f"Press {START_KEY} to start and hold {STOP_KEY} to close.")
 keyboard.wait(START_KEY)
 last_skill = None
+prev = time.time()
 while not keyboard.is_pressed(STOP_KEY):
     now = datetime.now().time()
 
     # Define a list of images and their associated actions
     actions = [
-        ('images/up.png', 'up'),
-        ('images/up2.png', 'up'),
-        ('images/left.png', 'left'),
-        ('images/left2.png', 'left'),
-        ('images/right.png', 'right'),
-        ('images/right2.png', 'right'),
-        ('images/pull.png', 'down'),
-        ('images/pull2.png', 'down'),
-        ('images/release.png', 'home'),
-        ('images/release2.png', 'home'),
+        ('images/up_.png', 'up'),
+        ('images/left_.png', 'left'),
+        ('images/right_.png', 'right'),
+        ('images/pull_.png', 'down'),
+        ('images/release_.png', 'home'),
     ]
-
     for image, action in actions:
-        conf = locateOnScreen(image, grayscale=True, region=REGION, confidence=CONFIDENCE)
+        conf = locateOnScreen(image, grayscale=False, region=REGION, confidence=CONFIDENCE)
         if conf is not None:
+            now = time.time()
             if last_skill != action:
+                print(f"Now-prev is {round(now-prev, 3)}, last_skill is {last_skill}, action is {action}")
                 keyboard.press_and_release(action)
                 if action == 'up' or action == 'down':  # double press for these actions
                     keyboard.press_and_release(action)
                 print(now, f"-> Performed action: {action}, image: {image[7:len(image)-4]}")
                 time.sleep(0.1)
                 last_skill = action
+                prev = now
                 break
+            elif now - prev > 5 and last_skill == action:
+                print(f"Now-prev is {round(now-prev, 3)}, last_skill is {last_skill}, action is {action}")
+                keyboard.press_and_release(action)
+                if action == 'up' or action == 'down':  # double press for these actions
+                    keyboard.press_and_release(action)
+                print(now, f"-> Performed action: {action}, image: {image[7:len(image)-4]}")
+                time.sleep(0.1)
+                last_skill = action
+                prev = now
+                break
+            
