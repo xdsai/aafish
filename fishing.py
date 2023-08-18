4bd12d2a1e7123ea1abeb31c33288380300cde7a
alex <skyycr@gmail.com>
2023-08-18 16:01:57 +0200

    lower threshold to 0.55

--- fishing.py
+++ fishing.py
@@ -7,7 +7,7 @@ from datetime import datetime
 START_KEY = '`'
 STOP_KEY = 'end'
 
-CONFIDENCE = 0.65
+CONFIDENCE = 0.55
 REGION=(841, 66,67, 34)
 
 print(f"Press {START_KEY} to start and hold {STOP_KEY} to close.")
